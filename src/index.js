4dcd6807859f1fdbbfcfee546b3a4c740323ec94
Matt Palmer <matt@silvercreekit.com>
2017-05-19 00:30:15 -0400

    Progress on style modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('babel-polyfill');
+require('./base.scss');
 const ey = require('./ey');
 
 console.log(ey());
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const Clean = require('clean-webpack-plugin');
 const Html = require('html-webpack-plugin');
+const ExtractText = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const dir = {
   src: path.resolve('src'),
@@ -10,7 +12,10 @@ const dir = {
   assets: path.resolve('assets'),
 };
 
-const base = {
+const styleBundle = new ExtractText('[name].css');
+
+
+const base = () => ({
   context: dir.src,
   entry: 'index.js',
   output: {
@@ -19,28 +24,58 @@ const base = {
   },
   resolve: {
     modules: [dir.src, 'node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         use: {
           loader: 'babel-loader',
           options: { cacheDirectory: true },
         },
       },
+      {
+        test: /\.scss$/,
+        use: styleBundle.extract([
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]),
+      },
     ],
   },
-};
+  plugins: [
+    styleBundle,
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: () => [autoprefixer(pkg.browserslist)],
+      },
+    }),
+  ],
+});
 
-const dev = {
+
+const dev = () => ({
   devtool: 'eval-source-map',
   plugins: [
+    new webpack.HotModuleReplacementPlugin({ multistep: true }),
     new Html({ template: path.join(dir.assets, 'index.html') }),
   ],
-};
+  devServer: {
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    stats: 'errors-only',
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+});
 
-const prod = {
+
+const prod = () => ({
   devtool: 'source-map',
   plugins: [
     new Clean(path.join(dir.dist, '**', '*'), { root: dir.dist }),
@@ -52,8 +87,9 @@ const prod = {
       },
     }),
   ],
-};
+});
+
 
 const environment = { dev, prod };
 
-module.exports = env => merge(base, environment[env]);
+module.exports = env => merge(base, environment[env](env));
