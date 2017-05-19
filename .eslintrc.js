c62f613fe7e98aeeff1c12a1368575be3206318e
Matt Palmer <matt@silvercreekit.com>
2017-05-19 02:59:45 -0400

    Add eslint globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   extends: 'airbnb',
   plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    worker: true,
+  },
+  globals: {
+    ENV: true,
+    VERSION: true,
+    SUPPORTED_BROWSERS: true,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 require('babel-polyfill');
 require('./base.scss');
-const ey = require('./ey');
 
-console.log(ey());
-console.log(ENV);
-console.log(VERSION);
-console.log(SUPPORTED_BROWSERS);
+console.log('It works!');
